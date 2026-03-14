b69b5152766a0f4768c09ffa125d0de797b2122d
Kimani Ann <akimani788@gmail.com>
2026-03-14 12:38:39 +0300

    initial commit

---
+++ intro.py
@@ -0,0 +1,17 @@
+
+def addtwonumbers():
+    num1=int(input("Enter the first number: "))
+    num2=int(input("Enter the second number: "))   
+    print (f"The sum of {num1} and {num2} is {num1 + num2}")
+
+# calling the function
+addtwonumbers() 
+
+
+def multiplytwonumbers():
+    num1=int(input("Enter the first number: "))
+    num2=int(input("Enter the second number: "))   
+    print (f"The product of {num1} and {num2} is {num1 * num2}")
+
+# calling the function
+multiplytwonumbers() 
